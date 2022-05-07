b4b3379f664523ab018aac943002f4b95678bec3
Abhuday <abhuday.mishra@hotmail.com>
2022-05-07 14:05:14 +0530

    Add: Frontend auth check to access dashboard

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
 import { AddTransaction } from "./AddTransaction";
 import { Balance } from "./Balance";
 import { Header } from "./Header";
 import { IncomeExpenses } from "./IncomeExpenses";
 import { TransactionList } from "./TransactionList";
+import { GlobalContext } from "../context/GlobalContext";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
+  const { user } = useContext(GlobalContext);
+  let navigate = useNavigate();
+  console.log(user);
+  useEffect(() => {
+    if (user && user._id) console.log(user);
+    else {
+      navigate("/login");
+    }
+  }, [user]);
   return (
     <>
       <Header />
